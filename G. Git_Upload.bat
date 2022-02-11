bb49c6ad201cec4df725b78e17c0eff31c0613ff
Burntmeat <50702772+eejongyun@users.noreply.github.com>
2022-02-11 17:47:17 +0900

    Delete G. Git_Upload.bat

--- G. Git_Upload.bat
+++
@@ -1,6 +0,0 @@
-::Git을 설치하고 나서 가장 먼저 해야 하는 것은 사용자이름과 이메일 주소를 설정하는 것이다
-git config --global user.name "eejongyun"
-git config --global user.email ee.jongyun@gmail.com
-
-::64비트 Windows 시스템에서 32비트 Notepad++을 설치했다면 `C:\Program Files (x86)`에 설치된다.
-git config --global core.editor "'C:/Program Files (x86)/Notepad++/notepad++.exe' -multiInst -nosession"
